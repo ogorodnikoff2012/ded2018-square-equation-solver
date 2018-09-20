e713e734230581983fbe259d6d76070874f95312
Vladimir Ogorodnikov <volodya5000@gmail.com>
2018-09-20 17:49:34 +0300

    My hands are typing words

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,8 @@
 #include <testing.h>
 #include <iostream>
+#include <solverapp.h>
+#include <sstream>
+#include <set>
 
 TEST_SET(SimpleTestSet) {
     TEST(HelloWorld) {
@@ -8,7 +11,27 @@ TEST_SET(SimpleTestSet) {
     };
 }
 
+TEST_SET(SolverAppSet) {
+    TEST(SimpleTest) {
+        std::stringstream data;
+        Console console(std::cin, data);
+        console.setVariable("verbosity", "ERROR");
+        SolverApp app(&console);
+        app.exec({"solve", "1", "0", "-1"});
+
+        std::cerr << "Stream: " << data.str() << std::endl;
+
+        std::set<double> answer;
+        double buffer;
+        while (data >> buffer) {
+            answer.insert(buffer);
+        }
+        return answer == std::set<double>{-1, 1};
+    };
+}
+
 int main() {
     test_autogen::SimpleTestSet().runTests();
+    test_autogen::SolverAppSet().runTests();
     return 0;
 }
